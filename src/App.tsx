7b1626d3eef09037574326ab73ebd04d7bb0e226
Facundo Agustín Fernández <facuassain@gmail.com>
2021-11-28 03:15:00 -0300

    Routes added
    /explore route added to show the ItemListContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { Navbar } from "./app/components";
 import { ItemListContainer } from "./features/tables";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-      <ItemListContainer title="Contenedor" />
-    </>
+
+      <Routes>
+        <Route path="/explore" element={<ItemListContainer title="Explore our products" />}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import CartWidget from "./CartWidget";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 
@@ -20,14 +21,14 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
+              <Link className="nav-link active" aria-current="page" to="/">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
+              <Link className="nav-link" to="/explore">
                 Explore
-              </a>
+              </Link>
             </li>
             <li className="nav-item dropdown">
               <a
