da30922c4f0aada3c90989e5ff0916c83b0cda2b
Facundo Agustín Fernández <facuassain@gmail.com>
2021-12-06 11:57:57 -0300

    Delete from cart functionality added

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { useAppSelector } from "../../app/hooks";
+import { Button } from "react-bootstrap";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { BsFillTrashFill } from "react-icons/bs";
+import { CartProduct, removeItem } from "../../features/cart/cartSlice";
 
 const Cart = () => {
   const cart = useAppSelector((state) => state.cart);
-
+  const dispatch = useAppDispatch();
+    const handleRemoveFromCart = (item : CartProduct) => {
+        dispatch(removeItem(item))
+    }
   return (
     <div className="my-5">
       {cart.products.map((cartItem) => (
@@ -11,6 +17,9 @@ const Cart = () => {
           <p>DESCRIPTION : {cartItem.description}</p>
           <p>PRICE : {cartItem.price}</p>
           <p>AMOUNT : {cartItem.amount}</p>
+          <Button variant="danger" onClick={() => handleRemoveFromCart(cartItem)}>
+            <BsFillTrashFill />
+          </Button>
           <hr />
         </>
       ))}
